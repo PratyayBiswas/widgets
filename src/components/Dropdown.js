4032627da750e9fbf11aae4c362dd73430da77ec
PratyayBiswas <pratyaybiswas.8@gmail.com>
2021-08-21 12:05:09 +0530

    first commit

---
+++ src/components/Dropdown.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Dropdown = ({ options }) => {
+
+    const renderedOptions = options.map((option) => {
+        return(
+            <div key={option.value} className="item">
+                {option.label}
+            </div>
+        );
+    });
+    return(
+        <div>Dropdown</div>
+    );
+};
+
+export default Dropdown;
